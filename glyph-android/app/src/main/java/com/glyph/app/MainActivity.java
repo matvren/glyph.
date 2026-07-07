8419d50a39093bcc24dd59c65d047e09b79be176
matvren <matvren@gmail.com>
2026-07-07 21:32:23 +0300

    Add files via upload

---
+++ glyph-android/app/src/main/java/com/glyph/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.glyph.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/glyph-word-game.html");
+    }
+}
